d2875cc3a4ca5a7e2c6d08187b897af459b18cf7
Shirantha Jayasooriya <skellumjayasooriya@gmail.com>
2024-08-17 09:57:04 +0530

    Creating login functionality

--- easy-teacher/src/main/java/com/example/easyteacher/security/jwt/JwtUtils.java
+++ easy-teacher/src/main/java/com/example/easyteacher/security/jwt/JwtUtils.java
@@ -1,16 +1,16 @@
 package com.example.easyteacher.security.jwt;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import com.example.easyteacher.security.sevices.UserDetailsImpl;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -42,5 +42,12 @@ public class JwtUtils {
 
     public String getUserNameFromJwtToken(String token) {
         return Jwts.parser().setSigningKey(key()).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public String generateJwtToken(Authentication authentication) {
+        UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
+        return Jwts.builder().subject((userPrincipal.getUsername())).issuedAt(new Date()).expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
     }
 }
--- easy-teacher/src/main/java/com/example/easyteacher/security/sevices/UserDetailsImpl.java
+++ easy-teacher/src/main/java/com/example/easyteacher/security/sevices/UserDetailsImpl.java
@@ -3,6 +3,7 @@ package com.example.easyteacher.security.sevices;
 import com.example.easyteacher.model.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @AllArgsConstructor
+@Getter
 public class UserDetailsImpl implements UserDetails {
 
     @Serial
