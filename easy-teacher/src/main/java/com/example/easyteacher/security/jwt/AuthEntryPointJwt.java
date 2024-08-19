be145067d3478b14d71827d7ec8545cd962da42c
Shirantha Jayasooriya <skellumjayasooriya@gmail.com>
2024-08-19 18:41:15 +0530

    Adding method level authorization

--- easy-teacher/src/main/java/com/example/easyteacher/security/jwt/AuthEntryPointJwt.java
+++ easy-teacher/src/main/java/com/example/easyteacher/security/jwt/AuthEntryPointJwt.java
@@ -23,7 +23,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
         log.error("Unauthorized error: {}", authException.getMessage());
 
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);;
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
         final Map<String, Object> body = new HashMap<>();
